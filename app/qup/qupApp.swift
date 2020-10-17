694fddb230d3f95f15ac1aa31a0c83779a7a8328
jalindogan <jeremiah.alindogan@gmail.com>
2020-10-17 13:13:50 -0700

    initial frontend push

---
+++ app/qup/qupApp.swift
@@ -0,0 +1,17 @@
+//
+//  qupApp.swift
+//  qup
+//
+//  Created by Jeremiah Alindogan on 10/17/20.
+//
+
+import SwiftUI
+
+@main
+struct qupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
