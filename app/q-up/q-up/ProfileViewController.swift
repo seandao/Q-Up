4c4f4e4f9eda3fe36ec00a53d5355eba5ba454d5
jalindogan <jeremiah.alindogan@gmail.com>
2020-10-17 17:32:54 -0700

    added tab bar controllers and some views

---
+++ app/q-up/q-up/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  q-up
+//
+//  Created by Jeremiah Alindogan on 10/17/20.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
